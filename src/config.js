4e099ed9794c1c9c22a50e2ddb52e59cb3c5b120
Mr Random <100831398+MrRandom23@users.noreply.github.com>
2023-09-20 11:54:14 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    token: process.env.token || "", // Your bot token
+    channelId: process.env.channelId || "", //Channel Id you want to send the message
+  
+    nodes: [
+      {
+        host: "", // Lavalink IP 
+        password: "", // Lavalink Password
+        port: 433, // Lavalink Port
+        retryDelay: 300000,
+        retryAmount: 25,
+        identifier: "", //Name of you lavalink 
+        secure: false,
+      },
+      ],
+  };
+  
